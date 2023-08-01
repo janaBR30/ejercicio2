a6d40f5d6bf18c08de3505b3db11ce0533deed61
Janaina Bacin <janabacrauber@gmail.com>
2023-08-01 14:18:21 -0300

    add Controller and Entity

---
+++ src/main/java/com/Ejercicio2/ejercicio2/Plato.java
@@ -0,0 +1,24 @@
+
+package com.Ejercicio2.ejercicio2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
+public class Plato {
+    private int num;
+    private String nombre;
+    private double precio;
+    private String descripcion;
+
+    public Plato(int num, String nombre, double precio, String descripcion) {
+        this.num = num;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public Plato() {
+    }
+}
---
+++ src/main/java/com/Ejercicio2/ejercicio2/controller/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Ejercicio2.ejercicio2.controller;
+
+/**
+ *
+ * @author janab
+ */
+public class Controller {
+    
+}
