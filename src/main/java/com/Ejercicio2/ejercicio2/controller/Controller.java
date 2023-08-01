94287e45e88203dfc425c8b4e9ae660b4c0b70fa
Janaina Bacin <janabacrauber@gmail.com>
2023-08-01 14:36:42 -0300

    created responseBody by id

--- src/main/java/com/Ejercicio2/ejercicio2/controller/Controller.java
+++ src/main/java/com/Ejercicio2/ejercicio2/controller/Controller.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    
     @GetMapping ("/platos/{num}")
     @ResponseBody
     public Plato traerPlatos (@PathVariable int num) {
